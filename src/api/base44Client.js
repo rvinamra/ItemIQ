0b1f5a6f31db5b15617b0e784a9afef2194aa905
Vinamra Rai <vinamravr1@gmail.com>
2025-10-07 12:17:52 -0400

    Add files via upload

---
+++ src/api/base44Client.js
@@ -0,0 +1,8 @@
+import { createClient } from '@base44/sdk';
+// import { getAccessToken } from '@base44/sdk/utils/auth-utils';
+
+// Create a client with authentication required
+export const base44 = createClient({
+  appId: "68d9ca8713f69a066d1f404d", 
+  requiresAuth: true // Ensure authentication is required for all operations
+});
---
+++ src/api/entities.js
@@ -0,0 +1,15 @@
+import { base44 } from './base44Client';
+
+
+export const Transaction = base44.entities.Transaction;
+
+export const MerchantProfile = base44.entities.MerchantProfile;
+
+export const CorporateExpense = base44.entities.CorporateExpense;
+
+export const Waitlist = base44.entities.Waitlist;
+
+
+
+// auth sdk:
+export const User = base44.auth;
